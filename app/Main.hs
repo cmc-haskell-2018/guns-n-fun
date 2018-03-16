fbd40a62a26b369a7a6d4561b124a8a0708d84fa
der0nidad <der0nidad@gmail.com>
2018-03-16 13:09:31 +0300

    Changed app/Main.hs

---
+++ app/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import GunsAndFun
+import Graphics.Gloss
+import Graphics.Gloss.Juicy
+import Types
+import Initialize
+import Render
+import Handle
+import Update
+
+main :: IO ()
+main = run
+
+
